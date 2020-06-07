b574190fa29212101cc97104a137cc9bec2c84a0
javier <javiermedmata@gmail.com>
2020-06-07 20:54:09 +0200

    Metodo bolsa 5

--- src/proyecto/tema/pkg4/productos.java
+++ src/proyecto/tema/pkg4/productos.java
@@ -121,4 +121,95 @@ public class productos {
         return total;
     }
 
+    //proceso para el dinero de la bolsa 4
+    public static double Bolsa4() {
+        double total = Packcocacola + chucherias + redBull + cocacola + BarraPan + oxigenoActivo + laysCampesinas;
+        total = total - descuento;
+        return total;
+    }
+    
+     //proceso para el dinero de la bolsa 5
+    public static double Bolsa5() {
+        double total = 0;
+        Scanner teclado = new Scanner(System.in);
+        //aqui vemos el numero de veces que va a tener que elegit un producto
+        System.out.println("Cuantos productos quiere");
+        int tamanoBolsa = teclado.nextInt();
+        
+        //aqui un bucle para que pueda elegir mas de un productos
+        for (int i = 1; i <= tamanoBolsa; i++) {
+            System.out.println("Inserte numero del producto\n"
+                    + "1.botella de monster \n"
+                    + "2. bolsa de patatas fritas \n"
+                    + "3.  pack de latas de coca-cola \n"
+                    + "4. bolsa de papa-delta \n"
+                    + "5. gusanitos RISI \n"
+                    + "6. paquete de medianoches \n"
+                    + "7. paquete de donuts \n"
+                    + "8.  bolsa de palomitas sabor ketchup \n"
+                    + "9. judías de lata merca-power \n"
+                    + "10. azúcar \n"
+                    + "11. bolsita de chucherías \n"
+                    + "12. red bull \n"
+                    + "13. coca-cola \n"
+                    + "14. 2 barras de pan \n"
+                    + "15. botella de oxígeno activo  \n"
+                    + "16.  lays sabor campesinas \n");
+            int numProducto = teclado.nextInt();
+            switch (numProducto) {
+                case 1:
+                    total = total + mounster;
+                    break;
+                case 2:
+                    total = total + patatasFritas;
+                    break;
+                case 3:
+                    total = total + Packcocacola;
+                    break;
+                case 4:
+                    total = total + papaDelta;
+                    break;
+                case 5:
+                    total = total + RISI;
+                    break;
+                case 6:
+                    total = total + medianoche;
+                    break;
+                case 7:
+                    total = total + donucts;
+                    break;
+                case 8:
+                    total = total + palomistasKetchup;
+                    break;
+                case 9:
+                    total = total + judias;
+                    break;
+                case 10:
+                    total = total + azucar;
+                    break;
+                case 11:
+                    total = total + chucherias;
+                    break;
+                case 12:
+                    total = total + redBull;
+                    break;
+                case 13:
+                    total = total + cocacola;
+                    break;
+                case 14:
+                    total = total + BarraPan;
+                    break;
+                case 15:
+                    total = total + oxigenoActivo;
+                    break;
+                case 16:
+                    total = total + laysCampesinas;
+                    break;
+
+            }
+        }
+        
+        return total;
+
+    }
 }

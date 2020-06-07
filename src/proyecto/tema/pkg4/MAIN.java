87ea8be0cec1d9097d757684d72725c146c120db
javier <javiermedmata@gmail.com>
2020-06-07 20:55:40 +0200

    MAin terminado

--- src/proyecto/tema/pkg4/MAIN.java
+++ src/proyecto/tema/pkg4/MAIN.java
@@ -5,7 +5,12 @@
  */
 package proyecto.tema.pkg4;
 
-
+import static proyecto.tema.pkg4.productos.Bolsa1;
+import static proyecto.tema.pkg4.productos.Bolsa2;
+import static proyecto.tema.pkg4.productos.Bolsa3;
+import static proyecto.tema.pkg4.productos.Bolsa4;
+import static proyecto.tema.pkg4.productos.Bolsa5;
+import java.util.Scanner;
 
 
 /**
@@ -16,7 +21,36 @@ public class MAIN {
 
     
     public static void main(String[] args) {
+         Scanner teclado = new Scanner(System.in);
+         //aqui preguntamos cual quiere elegir
+        System.out.println("inserte el numero de la bolsa que quiere \n"
+                + "bolsa 1: bolsita de chucherías, red bull, coca-cola, 2 barras de pan, botella de oxígeno activo, lays sabor campesinas, botella de monster \n"
+                + "bolsa 2: bolsa de patatas fritas, pack de latas de coca-cola, bolsa de papa-delta, gusanitos RISI, paquete de medianoches \n"
+                + "bolsa 3: bolsa de patatas fritas, pack de latas de coca-cola, paquete de donuts, bolsa de palomitas sabor ketchup, judías de lata merca-power, azúcar.\n"
+                + "bolsa 4: pack de latas de coca-cola, bolsita de chucherías, red bull, coca-cola, 2 barras de pan, botella de oxígeno activo, lays sabor campesinas \n"
+                + "bolsa 5: configurable pero no tendrá descuento. ");
+         int eleccion = teclado.nextInt();
          
+         //aqui es para que poner el proceso que necesita cada bolsa y el dinero final que tiene que pagar
+         
+         switch (eleccion){
+             case 1 :
+                 System.out.println("El precio de este producto es de "+Bolsa1());
+                 break;
+                  case 2 :
+                 System.out.println("El precio de este producto es de "+Bolsa2());
+                 break;
+                  case 3 :
+                 System.out.println("El precio de este producto es de "+Bolsa3());
+                 break;
+                  case 4 :
+                 System.out.println("El precio de este producto es de "+Bolsa4());
+                 break;
+                  case 5 :
+                 System.out.println("El precio de este producto es de "+Bolsa5());
+                 break;
+                  
+         }
     }
 
    
